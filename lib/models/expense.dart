589384d8e440f5d087144f01a1177ccb301f9eb2
HarshJivani08 <harshjivani140901@gmail.com>
2024-04-13 19:03:38 +0530

    initial commit

---
+++ lib/models/expense.dart
@@ -0,0 +1,9 @@
+class Expense {
+
+  Expense({required this.title,required this.amount , required this.dateTime , })
+
+  final int id;
+  final String title;
+  final double amount;
+  final DateTime dateTime;
+}
